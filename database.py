521d6e762947297726b230992301a888474b33ae
Pellel <algassimoupelleldiallo2005@gmail.com>
2023-10-27 22:35:44 +0000

    MISE a JOUR de la bdd

--- database.py
+++ database.py
@@ -97,42 +97,182 @@ class DatabaseManager:
         # Enregistrer les modifications
         self.conn.commit()
 
+    # fonction pour ajouter un utilisateur(eleves) dans la base de données
+    def insert_eleve(self, matricule, mot_de_passe, nom, prenom, age, classe_id, parent_1_telephone, parent_2_telephone):
+        self.cursor.execute('''INSERT INTO eleves (matricule, mot_de_passe, nom, prenom, age, classe_id, parent_1_telephone, parent_2_telephone) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+                               (matricule, generate_password_hash(mot_de_passe), nom, prenom, age, classe_id, parent_1_telephone, parent_2_telephone))
+        self.conn.commit()
+
     # Fonction pour ajouter un utilisateur(enseignant) dans la base de données
     def insert_enseignant(self, matricule, mot_de_passe, nom, prenom, telephone, email, materie_id):
         self.cursor.execute('''INSERT INTO enseignants (matricule, mot_de_passe, nom, prenom, telephone, email, materie_id) 
                                VALUES (?, ?, ?, ?, ?, ?, ?)''', (matricule, generate_password_hash(mot_de_passe), nom, prenom, telephone, email, materie_id))
         self.conn.commit()
 
-    # Fonction pour ajouter un utilisateur(eleve) dans la base de données
+    # Fonction pour ajouter un utilisateur(direction) dans la base de données
     def insert_direction(self, matricule, mot_de_passe, nom, prenom, email, telephone, poste):
         self.cursor.execute('''INSERT INTO direction (matricule, mot_de_passe, nom, prenom, email, telephone, poste) 
                                VALUES (?, ?, ?, ?, ?, ?, ?)''', (matricule, generate_password_hash(mot_de_passe), nom, prenom, email, telephone, poste))
         self.conn.commit()
 
     # Fonction pour récupérer un élève par son matricule dans la base de données
-    def get_eleve_by_matricule(self, matricule):
-        self.cursor.execute("SELECT * FROM eleves WHERE matricule=?", (matricule,))
-        eleve = self.cursor.fetchone()
-        if eleve:
-            return {'user_type': 'eleve', 'matricule': eleve[0], 'mot_de_passe': eleve[1], 'nom': eleve[2], 'prenom': eleve[3], 'age': eleve[4], 'classe_id': eleve[5], 'parent_1_telephone': eleve[6], 'parent_2_telephone': eleve[7]}
-        return None
+    def get_eleve_by_params(self, matricule=None, nom=None, prenom=None, classe_id=None):
+        query = "SELECT * FROM eleves WHERE"
+        params = []
 
-    # Fonction pour récupérer un enseignant par son matricule dans la base de données
-    def get_enseignant_by_matricule(self, matricule):
-        self.cursor.execute("SELECT * FROM enseignants WHERE matricule=?", (matricule,))
-        enseignant = self.cursor.fetchone()
-        if enseignant:
-            return {'user_type': 'enseignant', 'matricule': enseignant[0], 'mot_de_passe': enseignant[1], 'nom': enseignant[2], 'prenom': enseignant[3], 'telephone': enseignant[4], 'email': enseignant[5], 'materie_id': enseignant[6]}
-        return None
+        if matricule:
+            query += " matricule=? AND"
+            params.append(matricule)
+        if nom:
+            query += " nom=? AND"
+            params.append(nom)
+        if prenom:
+            query += " prenom=? AND"
+            params.append(prenom)
+        if classe_id:
+            query += " classe_id=? AND"
+            params.append(classe_id)
 
-    # Fonction pour​ récupérer un directeur par son matricule dans la base de données
-    def get_direction_by_matricule(self, matricule):
-        self.cursor.execute("SELECT * FROM direction WHERE matricule=?", (matricule,))
-        direction_member = self.cursor.fetchone()
-        if direction_member:
-            return {'user_type': 'direction', 'matricule': direction_member[0], 'mot_de_passe': direction_member[1], 'nom': direction_member[2], 'prenom': direction_member[3], 'email': direction_member[4], 'telephone': direction_member[5], 'poste': direction_member[6]}
-        return None
-    
+        # Supprime le dernier "AND" de la requête
+        query = query[:-4]
+
+        self.cursor.execute(query, tuple(params))
+        eleves = self.cursor.fetchall()
+
+        if eleves:
+            liste_eleves = []
+            for eleve in eleves:
+                eleve_info = {
+                    'user_type': 'eleve',
+                    'matricule': eleve[0],
+                    'mot_de_passe': eleve[1],
+                    'nom': eleve[2],
+                    'prenom': eleve[3],
+                    'age': eleve[4],
+                    'classe_id': eleve[5],
+                    'parent_1_telephone': eleve[6],
+                    'parent_2_telephone': eleve[7]
+                }
+                liste_eleves.append(eleve_info)
+            return liste_eleves
+        else:
+            return print("Aucun élève trouvé avec les critères spécifiés.")
+
+    # Fonction pour récupérer un enseignant par différents critères
+    def get_enseignant_by_params(self, matricule=None, nom=None, prenom=None, telephone=None):
+        query = "SELECT * FROM enseignants WHERE"
+        params = []
+
+        if matricule:
+            query += " matricule=? AND"
+            params.append(matricule)
+        if nom:
+            query += " nom=? AND"
+            params.append(nom)
+        if prenom:
+            query += " prenom=? AND"
+            params.append(prenom)
+        if telephone:
+            query += " telephone=? AND"
+            params.append(telephone)
+
+        # Supprime le dernier "AND" de la requête
+        query = query[:-4]
+
+        self.cursor.execute(query, tuple(params))
+        enseignants = self.cursor.fetchall()
+
+        if enseignants:
+            liste_enseignants = []
+            for enseignant in enseignants:
+                enseignant_info = {
+                    'user_type': 'enseignant',
+                    'matricule': enseignant[0],
+                    'mot_de_passe': enseignant[1],
+                    'nom': enseignant[2],
+                    'prenom': enseignant[3],
+                    'telephone': enseignant[4],
+                    'email': enseignant[5],
+                    'materie_id': enseignant[6]
+                }
+                liste_enseignants.append(enseignant_info)
+            return liste_enseignants
+        else:
+            return print("Aucun enseignant trouvé avec les critères spécifiés.")
+
+    # Fonction pour récupérer un membre de la direction par différents critères
+    def get_direction_by_params(self, matricule=None, nom=None, prenom=None, telephone=None):
+        query = "SELECT * FROM direction WHERE"
+        params = []
+
+        if matricule:
+            query += " matricule=? AND"
+            params.append(matricule)
+        if nom:
+            query += " nom=? AND"
+            params.append(nom)
+        if prenom:
+            query += " prenom=? AND"
+            params.append(prenom)
+        if telephone:
+            query += " telephone=? AND"
+            params.append(telephone)
+
+        # Supprime le dernier "AND" de la requête
+        query = query[:-4]
+
+        self.cursor.execute(query, tuple(params))
+        direction_members = self.cursor.fetchall()
+
+        if direction_members:
+            liste_direction = []
+            for direction_member in direction_members:
+                direction_info = {
+                    'user_type': 'direction',
+                    'matricule': direction_member[0],
+                    'mot_de_passe': direction_member[1],
+                    'nom': direction_member[2],
+                    'prenom': direction_member[3],
+                    'email': direction_member[4],
+                    'telephone': direction_member[5],
+                    'poste': direction_member[6]
+                }
+                liste_direction.append(direction_info)
+            return liste_direction
+        else:
+            return print("Aucun membre de la direction trouvé avec les critères spécifiés.")
+
+    # Fonction pour ajouter une classe dans la base de données
+    def insert_classe(self, niveau, nom, numero_salle, principal_matricule):
+        self.cursor.execute('''INSERT INTO classe (niveau, nom, numero_salle, principal_matricule) 
+                           VALUES (?, ?, ?, ?)''', (niveau, nom, numero_salle, principal_matricule))
+        self.conn.commit()
+
+    # Fonction pour ajouter une matière dans la base de données
+    def insert_matiere(self, nom):
+        self.cursor.execute('''INSERT INTO matiere (nom) VALUES (?)''', (nom,))
+        self.conn.commit()
+
+    # Fonction pour ajouter une note dans la base de données
+    def insert_note(self, eleve_matricule, matiere_id, note_1, note_2, note_3, trimestre):
+        self.cursor.execute('''INSERT INTO note (eleve_matricule, matiere_id, note_1, note_2, note_3, trimestre) 
+                               VALUES (?, ?, ?, ?, ?, ?)''', (eleve_matricule, matiere_id, note_1, note_2, note_3, trimestre))
+        self.conn.commit()
+
+    # Fonction pour ajouter une absence dans la base de données
+    def insert_absence(self, eleve_matricule, date_absence, motif_absence):
+        self.cursor.execute('''INSERT INTO absence (eleve_matricule, date_absence, motif_absence) 
+                               VALUES (?, ?, ?)''', (eleve_matricule, date_absence, motif_absence))
+        self.conn.commit()
+
+    # Fonction pour ajouter un retard dans la base de données
+    def insert_retard(self, eleve_matricule, date_retard, motif_retard):
+        self.cursor.execute('''INSERT INTO retard (eleve_matricule, date_retard, motif_retard) 
+                               VALUES (?, ?, ?)''', (eleve_matricule, date_retard, motif_retard))
+        self.conn.commit()
+
+
     # Fonction pour fermer la connexion avec la base de données
     def close_connection(self):
         self.conn.close()
