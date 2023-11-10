e9fe6441cc68c90df564e452781207e047e9dc13
Gaston-post <algassimoupelleldiallo2005@gmail.com>
2023-11-10 13:32:06 +0100

    Ajout des profs dans le systeme de login

--- auth.py
+++ auth.py
@@ -18,7 +18,7 @@ def login():
         role = request.form['role']
         # Vérifie si les champs matricule, password et role sont remplis
         if matricule and password and role != '':
-            # Vérifie le rôle de l'utilisateur (dans cet exemple, le rôle 'direction')
+            # Vérifie le rôle de l'utilisateur (direction ou enseignant ou eleve)
             if role == 'direction':
                 # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
                 with db_manager:
@@ -26,22 +26,42 @@ def login():
                     directionliste = db_manager.get_direction_by_params(matricule=matricule)
                     # Vérifie si l'utilisateur a été trouvé dans la base de données
                     if directionliste:
-                        direction = directionliste[0]
+                        directionMember = directionliste[0]
                         # Vérifie le mot de passe hashé
-                        print(direction)
-                        print(password)
-                        if check_password_hash(direction['mot_de_passe'], password) :
+                        if check_password_hash(directionMember['mot_de_passe'], password) :
                             # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
-                            return f"<p>DIRECTION</p><p> bonjour {direction['nom']}</p>"
+                            return f"<p>DIRECTION</p><p> bonjour {directionMember['nom']}</p>"
                         else:
                             # Mot de passe incorrect, affiche un message d'erreur
                             return render_template('login.html', message='Mot de passe incorrect')
                     else:
                         # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
-                        return render_template('login.html', message='Utilisateur non trouvé')
+                        return render_template('login.html', message='Utilisateur non trouvé')    
+            # Vérifier le role de l'utilisateur (direction ou enseignant ou eleve)
+            elif role == 'enseignant':
+                # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
+                with db_manager:
+                    # Récupère les informations de l'utilisateur (enseignant) à partir de la base de données
+                    enseignantliste = db_manager.get_enseignant_by_params(matricule=matricule)
+                    # Vérifie si l'utilisateur a été trouvé dans la base de données
+                    if enseignantliste:
+                        enseignant = enseignantliste[0]
+                        # Vérifie le mot de passe hashé
+                        print(enseignant)
+                        print(password)
+                        if check_password_hash(enseignant['mot_de_passe'], password) :
+                            # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
+                            return f"<p>DIRECTION</p><p> bonjour {enseignant['nom']}</p>"
+                        else:
+                            # Mot de passe incorrect, affiche un message d'erreur
+                            return render_template('login.html', message='Mot de passe incorrect')
+                    else:
+                        # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
+                        return render_template('login.html', message='Utilisateur non trouvé') 
         else:
             # Tous les champs ne sont pas remplis, affiche un message d'erreur
             return render_template('login.html', message='Veuillez remplir tous les champs')
+        
     else:
         # Si la requête n'est pas de type POST, affiche la page de formulaire de connexion
         return render_template('login.html', title='Connexion')
