4bca4265e269d3e893a736f34c34c1ffef818267
Gaston-post <algassimoupelleldiallo2005@gmail.com>
2023-11-28 15:29:31 +0100

    changement de dash.py

--- auth.py
+++ auth.py
@@ -8,60 +8,62 @@ auth_blueprint = Blueprint('auth', __name__)
 db_manager = DatabaseManager()
 
 # Autres routes d'authentification si nécessaire
-@auth_blueprint.route('/login', methods=['POST', 'GET'])
+@auth_blueprint.route('/login', )
+# methods=['POST', 'GET']
 def login():
     # Vérifie si la requête est de type POST
-    if request.method == 'POST':
+    # if request.method == 'POST':
         # Récupère les données du formulaire
-        matricule = request.form['matricule']
-        password = request.form['password']
-        role = request.form['role']
-        # Vérifie si les champs matricule, password et role sont remplis
-        if matricule and password and role != '':
-            # Vérifie le rôle de l'utilisateur (direction ou enseignant ou eleve)
-            if role == 'direction':
-                # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
-                with db_manager:
-                    # Récupère les informations de l'utilisateur (direction) à partir de la base de données
-                    directionliste = db_manager.get_direction_by_params(matricule=matricule)
-                    # Vérifie si l'utilisateur a été trouvé dans la base de données
-                    if directionliste:
-                        directionMember = directionliste[0]
-                        # Vérifie le mot de passe hashé
-                        if check_password_hash(directionMember['mot_de_passe'], password) :
-                            # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
-                            return f"<p>DIRECTION</p><p> bonjour {directionMember['nom']}</p>"
-                        else:
-                            # Mot de passe incorrect, affiche un message d'erreur
-                            return render_template('login.html', message='Mot de passe incorrect')
-                    else:
-                        # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
-                        return render_template('login.html', message='Utilisateur non trouvé')    
-            # Vérifier le role de l'utilisateur (direction ou enseignant ou eleve)
-            elif role == 'enseignant':
-                # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
-                with db_manager:
-                    # Récupère les informations de l'utilisateur (enseignant) à partir de la base de données
-                    enseignantliste = db_manager.get_enseignant_by_params(matricule=matricule)
-                    # Vérifie si l'utilisateur a été trouvé dans la base de données
-                    if enseignantliste:
-                        enseignant = enseignantliste[0]
-                        # Vérifie le mot de passe hashé
-                        print(enseignant)
-                        print(password)
-                        if check_password_hash(enseignant['mot_de_passe'], password) :
-                            # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
-                            return f"<p>DIRECTION</p><p> bonjour {enseignant['nom']}</p>"
-                        else:
-                            # Mot de passe incorrect, affiche un message d'erreur
-                            return render_template('login.html', message='Mot de passe incorrect')
-                    else:
-                        # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
-                        return render_template('login.html', message='Utilisateur non trouvé') 
-        else:
-            # Tous les champs ne sont pas remplis, affiche un message d'erreur
-            return render_template('login.html', message='Veuillez remplir tous les champs')
+    #     matricule = request.form['matricule']
+    #     password = request.form['password']
+    #     role = request.form['role']
+    #     # Vérifie si les champs matricule, password et role sont remplis
+    #     if matricule and password and role != '':
+    #         # Vérifie le rôle de l'utilisateur (direction ou enseignant ou eleve)
+    #         if role == 'direction':
+    #             # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
+    #             with db_manager:
+    #                 # Récupère les informations de l'utilisateur (direction) à partir de la base de données
+    #                 directionliste = db_manager.get_direction_by_params(matricule=matricule)
+    #                 # Vérifie si l'utilisateur a été trouvé dans la base de données
+    #                 if directionliste:
+    #                     directionMember = directionliste[0]
+    #                     # Vérifie le mot de passe hashé
+    #                     if check_password_hash(directionMember['mot_de_passe'], password) :
+    #                         # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
+    #                         return f"<p>DIRECTION</p><p> bonjour {directionMember['nom']}</p>"
+    #                     else:
+    #                         # Mot de passe incorrect, affiche un message d'erreur
+    #                         return render_template('login.html', message='Mot de passe incorrect')
+    #                 else:
+    #                     # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
+    #                     return render_template('login.html', message='Utilisateur non trouvé')    
+    #         # Vérifier le role de l'utilisateur (direction ou enseignant ou eleve)
+    #         elif role == 'enseignant':
+    #             # Utilisation du gestionnaire de contexte 'with' pour assurer une gestion appropriée de la connexion à la base de données
+    #             with db_manager:
+    #                 # Récupère les informations de l'utilisateur (enseignant) à partir de la base de données
+    #                 enseignantliste = db_manager.get_enseignant_by_params(matricule=matricule)
+    #                 # Vérifie si l'utilisateur a été trouvé dans la base de données
+    #                 if enseignantliste:
+    #                     enseignant = enseignantliste[0]
+    #                     # Vérifie le mot de passe hashé
+    #                     print(enseignant)
+    #                     print(password)
+    #                     if check_password_hash(enseignant['mot_de_passe'], password) :
+    #                         # Utilisateur authentifié, effectue la redirection vers le tableau de bord de la direction
+    #                         return f"<p>DIRECTION</p><p> bonjour {enseignant['nom']}</p>"
+    #                     else:
+    #                         # Mot de passe incorrect, affiche un message d'erreur
+    #                         return render_template('login.html', message='Mot de passe incorrect')
+    #                 else:
+    #                     # Utilisateur non trouvé dans la base de données, affiche un message d'erreur
+    #                     return render_template('login.html', message='Utilisateur non trouvé') 
+    #     else:
+    #         # Tous les champs ne sont pas remplis, affiche un message d'erreur
+    #         return render_template('login.html', message='Veuillez remplir tous les champs')
         
-    else:
-        # Si la requête n'est pas de type POST, affiche la page de formulaire de connexion
-        return render_template('login.html', title='Connexion')
+    # else:
+    #     # Si la requête n'est pas de type POST, affiche la page de formulaire de connexion
+    #     return render_template('login.html', title='Connexion')
+    return render_template('login.html', title='Connexion')
--- dashboard.py
+++ dashboard.py
@@ -3,21 +3,20 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 dashboard_blueprint = Blueprint('dashboard', __name__)
 
-@dashboard_blueprint.route('/eleve_dashboard')
+@dashboard_blueprint.route('/dashboard_Profs')
 def eleve_dashboard():
     # Code pour récupérer les notes, absences et retards de l'élève depuis la base de données
     # Afficher ces informations dans le modèle HTML
-    return "Tableau de bord de l'élève"
+    return  render_template('dashboard_Profs.html', title='dashboard_Profs')
 
 # Espace de gestion des notes pour les enseignants
-@dashboard_blueprint.route('/enseignant_dashboard')
+@dashboard_blueprint.route('/inscription_eleves')
 def enseignant_dashboard():
     # Code pour permettre aux enseignants de gérer les notes dans le modèle HTML
-    return "Tableau de bord de l'enseignant"
-
-# Page d'administration pour le personnel de direction
-@dashboard_blueprint.route('/direction_dashboard')
+    return render_template('inscription_eleves.html', title='dashboard_Profs')
+# Page inscription des amin
+@dashboard_blueprint.route('/inscription_admin.html')
 def direction_dashboard():
-    # Code pour permettre au personnel de direction de gérer les utilisateurs, générer des rapports
-    return"Tableau de bord de la direction"
+    # ....
+    return render_template('inscription_admin.html', title='dashboard_Profs')
 # Autres routes de tableau de bord si nécessaire
